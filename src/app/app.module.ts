6cc11412e4a26296bea76b8ea2e92fa3cc43cad2
Dinesh-db <dineshbabu.r56@gmail.com>
2025-10-29 10:25:18 +0530

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { BuildComponent } from './build/build.component';
+import { CartComponent } from './cart/cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrdersComponent,
+    BuildComponent,
+    CartComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
