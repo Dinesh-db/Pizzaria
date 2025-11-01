4e62dd6ba961a19d3cc3293a687cc1d131f7e89d
Dinesh-db <dineshbabu.r56@gmail.com>
2025-11-02 01:05:25 +0530

    Updating files

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import data from '../../assets/data.json';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent {
-
+  pizzas=data.pizzas;
+  constructor(){
+    console.log(this.pizzas);
+  }
 }
