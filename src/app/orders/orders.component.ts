799294265f1245759c75d109aecb9f32227a4bd7
Dinesh-db <dineshbabu.r56@gmail.com>
2025-11-20 12:06:53 +0530

    Receiving the data

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -8,11 +8,14 @@ import { ServiceService } from '../service.service';
 })
 export class OrdersComponent {
   constructor(private pc:ServiceService){
+    console.log(this.pizza);
   }
-
+  pizza:any;
   getdatas(){
     this.pc.getdata().subscribe((e)=>{
       console.log("data recieved",e);
+      this.pizza=e;
+      console.log(this.pizza);
     });
   }
 }
