a829a15352377fc19727daadb1b236a7426491b7
Dinesh-db <dineshbabu.r56@gmail.com>
2025-11-20 12:10:15 +0530

    adding changes

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -8,14 +8,13 @@ import { ServiceService } from '../service.service';
 })
 export class OrdersComponent {
   constructor(private pc:ServiceService){
+    this.pc.getdata().subscribe((e)=>{
+      this.pizza=e;
+    })
     console.log(this.pizza);
   }
   pizza:any;
   getdatas(){
-    this.pc.getdata().subscribe((e)=>{
-      console.log("data recieved",e);
-      this.pizza=e;
-      console.log(this.pizza);
-    });
+    console.log("data we get",this.pizza);
   }
 }
