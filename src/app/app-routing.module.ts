2f7c445ea0276248d8a3522e9961042dde60c08d
Dinesh-db <dineshbabu.r56@gmail.com>
2025-11-25 20:38:19 +0530

    Updating UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { OrdersComponent } from './orders/orders.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
   {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
   
 ];
 
